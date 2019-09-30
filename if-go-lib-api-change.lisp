2fcc79c242792465fdcf4251ff23b5d2c31eb1d4
ccQpein <ccqpein@protonmail.com>
2019-09-30 09:37:15 -0400

    a bit

--- if-go-lib-api-change.lisp
+++ if-go-lib-api-change.lisp
@@ -29,26 +29,43 @@
   return-value)
 
 (defun give-func-declare (line)
-  (let ((rest (cdr (cl-ppcre:split "\\s+" line :limit 2)))) ; clean "func"
-    (if (char= #\( (elt (car rest) 0))
+  (let ((rest (cadr (cl-ppcre:split "\\s+" line :limit 2)))) ; clean "func"
+    (print rest)
+    (if (char= #\( (elt rest 0))
         ;; make method
         (let (which-type
               name
               args
               return-value
               temp)
+          (print temp)
           (setf temp (cl-ppcre:split "(?<=\\))\\s+" rest :limit 2))
+          (print temp)
+          ;; temp is ("(a int)", "funcname() return {")
           (setf which-type (value-and-types (car temp)))
-
-          (setf temp (cl-ppcre:split "(?<=\\))\\s+" (cadr temp) :limit 2))
-          (make-method))
+          
+          (setf temp (cadr temp))
+          (setf name (cl-ppcre:scan-to-strings "\\b\\w+" temp))
+          (print temp)
+          
+          (setf temp (cadr (cl-ppcre:split "\\b\\w+" temp :limit 2)))
+          ;;temp is "(a int, b int) return {", function args and return type now
+          (setf temp (cl-ppcre:split "(?<=\\))\\s+" temp  :limit 2))
+          ;;temp is list now
+          (setf args (value-and-types (car temp)))
+          (setf temp (cadr temp))
+          ;;temp is only return value now
+          (setf temp (car (cl-ppcre:split "\\s*{" temp)))
+          (print temp)
+          ;;(make-go-method)
+          )
         ;; make function
-        
+        ;;(make-go-function)
         )))
 
 (defun value-and-types (wrap-code)
   "wrap-code is (a int, b int, c ...interface{})"
-  
+  ""
   )
 
 
