3bec648df31fffecc4f7a89e11a47709da481e66
ccQpein <ccqpein@protonmail.com>
2019-10-02 17:27:34 -0400

    finish demo

--- if-go-lib-api-change.lisp
+++ if-go-lib-api-change.lisp
@@ -1,26 +1,36 @@
-(ql:quickload :cl-ppcre)
-(setf )
+(eval-when (:execute :load-toplevel :compile-toplevel)
+  (load "~/quicklisp/setup.lisp")
+  (ql:quickload :cl-ppcre)
 
-(defun scan-file (filepath)
-  "read a file and return parse result"
+  (defpackage #:if-go-lib-api-change
+    (:use #:cl #:cl-ppcre))
+
+  (in-package #:if-go-lib-api-change))
+
+;;;;:= TODO: need know which package
+;;;;:= TODO: need pub/pravite judger
+;;;;:= TODO: need somewhere to store results for next operation
+;;;;:= TODO: need to handle "a,b,c int" format in struct
+
+(setf *print-case* :capitalize)
+
+
+(defstruct go-package
+  name)
+
+
+(defstruct go-type
+  name
+  type
+  fields
   )
 
-(defun scan-string (stream)
-  (let* ((str (read-line stream))
-         (this-line (cl-ppcre:split "\\s+" str :limit 2))))
-  (cond ((string= "type" (car this-line))
-         (concatenate 'string str (find-this-block stream))
-         )
-        ((equal "func" (car this-line))
-         (give-func-declare str)
-         )
-        )
-  )
 
 (defstruct go-function
   name
   args
   return-value)
+
 
 (defstruct go-method
   type
@@ -28,9 +38,76 @@
   args
   return-value)
 
+
+(defun scan-file (filepath)
+  "read a file and return parse result"
+  (with-open-file (s filepath)
+    (scan-code s))
+  )
+
+
+(defun scan-code (stream)
+  (do* ((result '())
+       (str (read-line stream nil) (read-line stream nil))
+       (this-line (cl-ppcre:split "\\s+|\\(" str :limit 2) (cl-ppcre:split "\\s+|\\(" str :limit 2) ))
+       ((not str) (remove nil result))
+    (if str
+        (setf result
+              (append result
+                      (list
+                       (cond ((string= "type" (car this-line))
+                              ;; find when this code block end, merge it with first line to get whole
+                              ;; code block 
+                              (let ((code-block (concatenate 'string str (find-this-block stream))))
+                                (with-input-from-string (ll code-block)
+                                  (let* ((first-line (read-line ll))
+                                         (type-is (cl-ppcre:scan-to-strings "\\w+(?=\\s*{\\s*$)" first-line))) ; first line
+                                    (cond ((string= "struct" type-is)
+                                           (let ((type-instance (make-go-type :name "" :type type-is :fields '())))
+                                             ;; find name
+                                             (setf (go-type-name type-instance)
+                                                   (nth 1 (cl-ppcre:split "\\s+" first-line)))
+                                             ;; read all line left
+                                             (do* ((li (read-line ll nil) (read-line ll nil))
+                                                   (cut-li (cl-ppcre:split "\\s+" li))
+                                                   (result '()))
+                                                  ((not li)
+                                                   (setf (go-type-fields type-instance) result)
+                                                   (return type-instance))
+                                               (if (not (equal "}" li))
+                                                   (setf result (append result
+                                                                        (list (remove-if (lambda (x) (string= x ""))
+                                                                                         cut-li))))))))
+                                          ((string= "interface" type-is)
+                                           (let ((type-instance (make-go-type :name "" :type type-is :fields '())))
+                                             ;; find name
+                                             (setf (go-type-name type-instance)
+                                                   (nth 1 (cl-ppcre:split "\\s+" first-line)))
+                                             ;; read all line left
+                                             (do* ((li (read-line ll nil) (read-line ll nil))
+                                                   (cut-li (cl-ppcre:split "\\s+" li :limit 2))
+                                                   (result '()))
+                                                  ((not li)
+                                                   (setf (go-type-fields type-instance) result)
+                                                   (return type-instance))
+                                               (if (not (equal "}" li))
+                                                   (setf result (append result
+                                                                        (list (remove-if (lambda (x) (string= x ""))
+                                                                                         cut-li)))))))
+                                           )))
+                                  )))
+                             ((string= "func" (car this-line))
+                              (give-func-declare str)
+                              )
+                             ((string= "package" (car this-line))
+                              (make-go-package :name (cadr this-line)))
+                             )))))))
+
+
 (defun give-func-declare (line)
-  (let ((rest (cadr (cl-ppcre:split "\\s+" line :limit 2)))) ; clean "func"
-    (print rest)
+  "return method and function struct"
+  (declare (string line))
+  (let ((rest (cadr (cl-ppcre:split "\\w+\\s*(?=(\\w+|\\())" line :limit 2)))) ; clean "func"
     (if (char= #\( (elt rest 0))
         ;; make method
         (let (which-type
@@ -38,43 +115,61 @@
               args
               return-value
               temp)
-          (print temp)
-          (setf temp (cl-ppcre:split "(?<=\\))\\s+" rest :limit 2))
-          (print temp)
-          ;; temp is ("(a int)", "funcname() return {")
-          (setf which-type (value-and-types (car temp)))
-          
-          (setf temp (cadr temp))
-          (setf name (cl-ppcre:scan-to-strings "\\b\\w+" temp))
-          (print temp)
-          
-          (setf temp (cadr (cl-ppcre:split "\\b\\w+" temp :limit 2)))
-          ;;temp is "(a int, b int) return {", function args and return type now
-          (setf temp (cl-ppcre:split "(?<=\\))\\s+" temp  :limit 2))
-          ;;temp is list now
-          (setf args (value-and-types (car temp)))
-          (setf temp (cadr temp))
-          ;;temp is only return value now
-          (setf temp (car (cl-ppcre:split "\\s*{" temp)))
-          (print temp)
-          ;;(make-go-method)
+          (setf
+           ;; update temp to (type, rest)
+           temp (cl-ppcre:split "(?<=\\))\\s*" rest :limit 2)
+           ;; give which-type value
+           which-type (value-and-types (car temp))
+           ;; update temp to rest string
+           temp (cadr temp)
+           ;; give method name
+           name (cl-ppcre:scan-to-strings "\\b\\w+" temp)
+           ;; update temp to string without method name
+           temp (cadr (cl-ppcre:split "\\b\\w+\\s+" temp :limit 2))
+           ;; give args value
+           args (value-and-types (cl-ppcre:scan-to-strings "\\(.*?\\)" temp))
+           ;; temp only have return part now
+           temp (cadr (cl-ppcre:split "\\(.*?\\)\\s*" temp  :limit 2))
+           return-value (value-and-types (cl-ppcre:scan-to-strings ".*(?=\s*{)" temp))
+           )
+
+          (make-go-method :type which-type :name name :args args :return-value return-value)
           )
         ;; make function
-        ;;(make-go-function)
+        (let (name args return-value (temp rest))
+          (setf
+           ;;give function name
+           name (cl-ppcre:scan-to-strings "\\b\\w+" temp)
+           ;; update temp to string without method name
+           temp (cadr (cl-ppcre:split "\\b\\w+\\s+" temp :limit 2))
+           ;; give args value
+           args (value-and-types (cl-ppcre:scan-to-strings "\\(.*\\)" temp))
+           ;; temp only have return part now
+           temp (cadr (cl-ppcre:split "\\(.*?\\)\\s*" temp  :limit 2))
+           return-value (value-and-types (cl-ppcre:scan-to-strings ".*(?=\s*{)" temp))
+           )
+          (make-go-function :name name :args args :return-value return-value)
+          )
         )))
+
 
 (defun value-and-types (wrap-code)
   "wrap-code is (a int, b int, c ...interface{})"
-  ""
-  )
+  (let* ((clean-parameter-code (cl-ppcre:scan-to-strings "(?<=\\().*(?=\\))" wrap-code))
+         (group (cl-ppcre:split "\\s*,\\s*" clean-parameter-code)))
+    (loop
+       for pair in group
+       for (v ty) = (cl-ppcre:split "\\s+" pair)
+       collect (cons v ty))
+    ))
 
 
 (defun find-this-block (stream)
   "find code body block"
   (let ((stack '(#\{))
-        (result '()))
+        (result '(#\linefeed)))
     (do (c)
-        ((not stack) (return (coerce result 'string)))
+        ((not stack) (coerce result 'string))
       ;; update c and result
       (setf c (read-char stream)
             result (append result (list c)))
@@ -83,3 +178,4 @@
              (setf stack (butlast stack)))
             ((char= c #\{)
              (setf stack (append stack (list c))))))))
+
